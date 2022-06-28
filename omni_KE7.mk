7222ec7fe7df5af7ebd7d96366ca853723ae47f7
Parvej899 <107097395+Parvej899@users.noreply.github.com>
2022-06-28 09:16:17 +0600

    Tecno KE7 DEVICE TREE

---
+++ omni_KE7.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := KE7
+PRODUCT_NAME := omni_KE7
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KE7
+PRODUCT_MANUFACTURER := TECNO MOBILITY
+
+
+# Dynamic partition
+PRODUCT_USE_DYNAMIC_PARTITIOS := true
